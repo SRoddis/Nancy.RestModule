d94468ea1b02c77d4751d2069d7f277f3a1b729a
sroddis <sean_roddis@hotmail.de>
2017-07-03 19:32:57 +0200

    fixed naming of test.dll

--- post_build.cmd
+++ post_build.cmd
@@ -2,6 +2,6 @@ nuget install NUnit.Runners -Version 3.2.1 -OutputDirectory tools
 nuget install OpenCover -Version 4.6.519 -OutputDirectory tools
 nuget install coveralls.net -Version 0.6.0 -OutputDirectory tools
  
-.\tools\OpenCover.4.6.519\tools\OpenCover.Console.exe -target:.\tools\NUnit.ConsoleRunner.3.2.1\tools\nunit3-console.exe -targetargs:".\FlyingMigratory.Test\bin\Debug\FlyingMigratory.Test.dll" -filter:"+[FlyingMigratory]* -[FlyingMigratory.Test]*" -register:user
+.\tools\OpenCover.4.6.519\tools\OpenCover.Console.exe -target:.\tools\NUnit.ConsoleRunner.3.2.1\tools\nunit3-console.exe -targetargs:".\Nancy.RestModule.Test\bin\Release\Nancy.RestModule.Test.dll" -filter:"+[Nancy.RestModule]* -[Nancy.RestModule.Test]*" -register:user
  
 .\tools\coveralls.net.0.6.0\tools\csmacnz.Coveralls.exe --opencover -i .\results.xml
